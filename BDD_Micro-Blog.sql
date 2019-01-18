319855816494a1acc79f6ebdd076f9509c5590ed
Damien <dc.developpeur@gmail.com>
2019-01-18 23:29:23 +0100

    commentaires sur login

--- BDD_Micro-Blog.sql
+++ BDD_Micro-Blog.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
--- Client :  127.0.0.1
--- Généré le :  Ven 18 Janvier 2019 à 19:06
--- Version du serveur :  5.7.14
--- Version de PHP :  5.6.25
+-- Hôte : localhost
+-- Généré le :  ven. 18 jan. 2019 à 22:05
+-- Version du serveur :  5.7.23-23-log
+-- Version de PHP :  7.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,10 +19,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `micro_blog`
+-- Base de données :  `damien-commard`
 --
-CREATE DATABASE IF NOT EXISTS `micro_blog` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `micro_blog`;
 
 -- --------------------------------------------------------
 
@@ -37,40 +37,14 @@ CREATE TABLE `messages` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `messages`
+-- Déchargement des données de la table `messages`
 --
 
 INSERT INTO `messages` (`id`, `contenu`, `date`, `votes`, `adresse_ip`) VALUES
-(1, 'jeremy est un crack', 1547554498, 4, '::1'),
-(80, 'c\'est pour bientÃ´t', 1547726548, 0, NULL),
-(83, 'alexandre est un gÃ©nie', 1547726580, 1, '::1'),
-(82, 'derniÃ¨re tentative\r\n', 1547726569, 1, '::1'),
-(81, 'allez on y est presque', 1547726558, 0, NULL),
-(79, 'on arrive au bout', 1547680686, 1, '::1');
-
--- --------------------------------------------------------
+(85, 'Bonjour Mr LANNOY et Bienvenue  sur le Micro-Blog de Damien COMMARD ', 1547843123, 0, NULL);
 
 --
--- Structure de la table `user`
---
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `login` varchar(32) NOT NULL,
-  `pwd` varchar(32) NOT NULL,
-  `sid` varchar(128) NOT NULL,
-  `email` varchar(64) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
---
--- Contenu de la table `user`
---
-
-INSERT INTO `user` (`id`, `login`, `pwd`, `sid`, `email`) VALUES
-(3, 'damien', 'damien', 'd799335f1d45e7611fa0d71fe938795c', 'damien.commard@gmail.com');
-
---
--- Index pour les tables exportées
+-- Index pour les tables déchargées
 --
 
 --
@@ -80,25 +54,16 @@ ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
 -- AUTO_INCREMENT pour la table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
---
--- AUTO_INCREMENT pour la table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
